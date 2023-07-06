375368943ce76090049f959c84725b280a44bd05
Bruno Carvalho da Costa <brunoccst@gmail.com>
2023-07-06 13:46:09 +0200

    Added useQuery to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { wait } from 'utils';
+
+const POSTS = [
+  { id: crypto.randomUUID(), title: "Post 1" },
+  { id: crypto.randomUUID(), title: "Post 2 " }
+]
 
 function App() {
+  // Queries: getting data from somewhere
+  const postQuery = useQuery({
+    queryKey: ["posts"], // Unique identifier for the query
+    queryFn: () => wait(1000).then(() => [...POSTS]) // Runs to actually query the data
+  })
+
+  if (postQuery.isLoading) return <h1>Loading...</h1>
+  if (postQuery.isError) return <pre>{JSON.stringify(postQuery.error)}</pre>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {postQuery.data.map(post => (
+        <div key={post.id}>{post.title}</div>
+      ))}
     </div>
   );
 }
